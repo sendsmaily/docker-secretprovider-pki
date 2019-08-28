e1e866155d042bcb710c347eec8e20a1384bb5ff
Janar Todesk <janar@smaily.com>
2019-08-28 14:19:21 +0300

    Add main entrypoint for the PKI provider

---
+++ go.mod
@@ -0,0 +1,23 @@
+module docker-secretprovider-pki
+
+go 1.12
+
+require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/coreos/go-systemd v0.0.0-20190620071333-e64a0ec8b42a // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-plugins-helpers v0.0.0-20181025120712-1e6269c305b8
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/onsi/ginkgo v1.9.0
+	github.com/onsi/gomega v1.6.0
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/secrethub/secrethub-go v0.20.0
+	github.com/stretchr/testify v1.4.0 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/docker/docker/client"
+	"github.com/docker/go-plugins-helpers/secrets"
+	"go.uber.org/zap"
+
+	"docker-secretprovider-pki/backend"
+	"docker-secretprovider-pki/driver"
+)
+
+func main() {
+	logger, _ := zap.NewDevelopment()
+	zap.ReplaceGlobals(logger)
+
+	zap.S().Info("pki: initializing...")
+
+	var httpClient *http.Client
+	dockerClient, err := client.NewClient("unix:///docker.sock", "1.35", httpClient, nil)
+	if err != nil {
+		zap.S().Fatalf("pki: error creating docker client: %v", err)
+	}
+
+	var ca driver.CABackend
+	switch os.Getenv("BACKEND") {
+	case "secrethub":
+		ca, err = backend.NewSecrethubBackend()
+	default:
+		ca, err = backend.NewTestBackend()
+	}
+
+	if err != nil {
+		zap.S().Fatalf("pki: error initializing CA backend: %s", err)
+	}
+
+	drv, err := driver.NewDriver(ca, dockerClient)
+	if err != nil {
+		zap.S().Fatalf("pki: error initializing PKI driver: %s", err)
+	}
+
+	handler := secrets.NewHandler(drv)
+	if err := handler.ServeUnix("plugin", 0); err != nil {
+		zap.S().Fatalf("pki: %s", err)
+	}
+}
